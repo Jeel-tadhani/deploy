1fd0ec1878b7d885d82fb1462956d7292ad9d1da
jeel <jeeltadhani3@gmail.com>
2023-11-30 00:10:26 +0530

    s

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require("dotenv").config()
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
